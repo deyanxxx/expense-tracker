baf71ace7f18beb339bad9c5670ac320598dfd25
deyanxxx <itsandreadb@gmail.com>
2024-04-21 01:03:12 +0800

    banner

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './index.css';
 
 function App() {
   return (
-    <div>
       <Home />
-    </div>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,29 @@
 import React from 'react';
+import { ReactComponent as Banner } from '../assets/banner.svg';
 
 function Home() {
   return (
-    <div className='flex flex-col items-center p-16'>
-      <div className='flex justify-center font-bold text-4xl'>
-        <h1 style={{ color: '#30343F' }}>Welcome to </h1>
-        {' '}
-        {/* Space */}
-        <h1 style={{ color: '#273469' }} className='ml-2'>Expense Tracker</h1>
+    <div className='flex flex-col items-center p-10'>
+      <div className='flex justify-center font-bold text-4xl pt-10'>
+        <h1 style={{ color: '#30343F' }}>Welcome to <span style={{ color: '#273469' }} >Expense Tracker</span></h1>
       </div>
       <h3 className='text-center p-2 font-md'>Track and manage your expenses at ease</h3>
       <div className='flex flex-row items-center pt-10'>
         <div className='p-4'>
-        <button style={{ color: '#273469', borderColor: '#273469'}} className='border-2 rounded-md px-14 py-4 font-semibold'>Log In</button>
+           
+                <button style={{ color: '#273469', borderColor: '#273469'}} className='border-2 rounded-md px-14 py-4 font-semibold'>
+                    Log In
+                </button>
+            
         </div>
         <div>
-        <button style={{ backgroundColor: '#273469', borderColor: '#273469'}} className='rounded-md border-2 px-14 py-4 text-white font-semibold'>Sign Up</button>
+            
+                <button style={{ backgroundColor: '#273469', borderColor: '#273469'}} className='rounded-md border-2 px-14 py-4 text-white font-semibold'>
+                    Sign Up
+                </button>
         </div>
       </div>
+      <Banner className='w-92 h-80 pt-10' />
     </div>
   )
 }
