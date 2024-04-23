85291dcf58e825d7e84cf817849c4ea4c6cf79e3
deyanxxx <itsandreadb@gmail.com>
2024-04-23 16:05:17 +0800

    usenavigate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import Home from "./pages/Home";
 import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 
 function App() {
   return (
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import { ReactComponent as Banner } from '../assets/banner.svg';
+import { useNavigate } from "react-router-dom";
+
 
 function Home() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/Login');
+  };
+
+  const handleSignup = () => {
+    navigate('/Signup');
+  };
+
   return (
     <div className='flex flex-col items-center p-10'>
       <div className='flex justify-center font-bold text-4xl pt-10'>
@@ -10,17 +22,16 @@ function Home() {
       <h3 className='text-center p-2 font-md'>Track and manage your expenses at ease</h3>
       <div className='flex flex-row items-center pt-10'>
         <div className='p-4'>
-           
-                <button style={{ color: '#273469', borderColor: '#273469'}} className='border-2 rounded-md px-14 py-4 font-semibold'>
-                    Log In
-                </button>
-            
+          <button style={{ color: '#273469', borderColor: '#273469'}} className='border-2 rounded-md px-14 py-4 font-semibold'
+          onClick={handleLogin}>
+            Log In
+          </button>
         </div>
         <div>
-            
-                <button style={{ backgroundColor: '#273469', borderColor: '#273469'}} className='rounded-md border-2 px-14 py-4 text-white font-semibold'>
-                    Sign Up
-                </button>
+          <button style={{ backgroundColor: '#273469', borderColor: '#273469'}} className='rounded-md border-2 px-14 py-4 text-white font-semibold'
+          onClick={handleSignup}>
+            Sign Up
+          </button>
         </div>
       </div>
       <Banner className='w-92 h-80 pt-10' />
